c0bfada886ef003ad0d027088fb5b257efe25482
isaacedubaron <143570098+isaacedubaron@users.noreply.github.com>
2024-02-19 11:04:03 -0600

    Add files via upload

---
+++ Atomic.Forms.ISB.sql
@@ -0,0 +1,23 @@
+#bits
+#1. Bonnies Beautiful Boutique, Nick Yates, Laura Howler, MarketPoint Sales, Jarod Hershey, Sean Goduto, Jason Salazar, Fishermen's spot shop, Lindsey Semour.
+#3 67424-6749
+#4 9/14/2018, 9/24/2018
+#7 18,746.05
+#11 Email, Preffered mailing address, Phone number
+
+#Colonial
+
+#4 Bradbury Mountain ride, Mount Battie ride, Park loop ride, Westfield River loop
+#5 MT. Ascutney, Lake Memp tour, Missisqoui River
+#9 Cadillac Mountain ride, Bradbury Mountain ride,Mount Biddie ride, Westfield River loop
+#13 Liam Northfold, Wachusett Mountain : Hiking; Long Pong: Hiking//Arnold Ocean, Mt. Ascutney North Peak : Hiking; Mt.Ascutney West Peak: Hiking// Karen Busa, Mount Battie Ride : Biking; Mount Garfield Hike: Hiking//Clement Chau, Cadillac Mountain Ride : Biking ; Long pond : Hiking//Boral Bretton Chocorua, Lake Tour : Paddling Sawyer River Ride : Biking//Sadie Gernowski, Mt. Cardigan - Firescrew : Hiking; Bradbury Mountain Ride: Biking//Ryan Goff, Crawford Path Presidentials hike : Hiking ; Mount Cardigan Hike : Hiking
+
+#sports
+
+#4 Electrical stimulation and aquatic therapy
+#6 Steven Wilder
+#8 Ultrasound
+#10 Hourly rate, They could include offdays, avg amount of billed hours, possibility of maternity and paternity absenses
+
+#Review
+#7 When sorting multiple columns at the same time, the first couloumn that is sorted will alwasy be the primary key// Guide number, Trip ID, Customer Number, Reservation ID, Trip ID
